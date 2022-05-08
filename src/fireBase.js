f0945f37f8be107140892d5b9a2e57ca189054e4
akshatgupta105 <53713926+Dev-AkshatGupta@users.noreply.github.com>
2022-05-08 19:50:13 +0530

    env created-akshat

--- src/fireBase.js
+++ src/fireBase.js
@@ -3,16 +3,24 @@ import { initializeApp } from "firebase/app";
 import {getAuth} from "firebase/auth";
 import {getFirestore,doc,collection,setDoc} from "@firebase/firestore";
 
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCBWQtUsLQmot6hePXZc-tE9mco-YpcWf0",
+//   authDomain: "tracker-63593.firebaseapp.com",
+//   projectId: "tracker-63593",
+//   storageBucket: "tracker-63593.appspot.com",
+//   messagingSenderId: "583576596934",
+//   appId: "1:583576596934:web:84b8acc36bc5aae768c2c7",
+//   measurementId: "G-W30CMZR8C4"
+// };
 const firebaseConfig = {
-  apiKey: "AIzaSyCBWQtUsLQmot6hePXZc-tE9mco-YpcWf0",
-  authDomain: "tracker-63593.firebaseapp.com",
-  projectId: "tracker-63593",
-  storageBucket: "tracker-63593.appspot.com",
-  messagingSenderId: "583576596934",
-  appId: "1:583576596934:web:84b8acc36bc5aae768c2c7",
-  measurementId: "G-W30CMZR8C4"
+apiKey:process.env.REACT_APP_apiKey,
+  authDomain:process.env.REACT_APP_authDomain,
+  projectId:process.env.REACT_APP_projectId,
+  storageBucket:process.env.REACT_APP_storageBucket ,
+  messagingSenderId:process.env.REACT_APP_messagingSenderId,
+  appId:process.env.REACT_APP_appId,
+  measurementId:process.env.REACT_APP_measurementId
 };
-
 
 
 const app = initializeApp(firebaseConfig);
