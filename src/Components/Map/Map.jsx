73ea99d451fbeaeb90957fc3b4082409a54ea627
arjun <arjun2000raj@gmail.com>
2022-05-08 19:26:46 +0530

    Feat : Added maps to the project

---
+++ src/Components/Map/Map.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react'
+import GoogleMapReact from "google-map-react"
+import { useSelector } from 'react-redux'
+import { Marker } from '../Marker/Marker'
+
+export const Map = () => {
+    const [coordinates, setCoordinates] = useState({ lat: 25.5941, lng: 85.1376 })
+    const { cngPump } = useSelector(state => state.pumps)
+
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(({ coords: { latitude, longitude } }) => {
+            setCoordinates({ lat: latitude, lng: longitude })
+        })
+    }, [])
+
+    return (
+        <GoogleMapReact
+            bootstrapURLKeys={{ key: 'AIzaSyB796fndw1WdQUItbShmuWyy-m7DGC5NzQ' }}
+            defaultCenter={coordinates}
+            center={coordinates}
+            defaultZoom={15}
+            margin={[50, 50, 50, 50]}
+        >
+            {cngPump.map((place, i) =>
+                <Marker
+                    key={i}
+                    lat={place.latitude}
+                    lng={place.longitude}
+                    text={place.name}
+                />)}
+        </GoogleMapReact>
+    )
+}
