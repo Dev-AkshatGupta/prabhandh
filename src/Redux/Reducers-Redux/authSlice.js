ba669861c8444f9835265a2e661b7c4f2067bfd9
akshatgupta105 <53713926+Dev-AkshatGupta@users.noreply.github.com>
2022-05-09 02:39:49 +0530

    refactoring

--- src/Redux/Reducers-Redux/authSlice.js
+++ src/Redux/Reducers-Redux/authSlice.js
@@ -1,97 +1,104 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import {
-    createUserWithEmailAndPassword,
-    signInWithEmailAndPassword,
-    onAuthStateChanged,
-    signOut,
-    GoogleAuthProvider,
-    signInWithPopup,
-
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+  GoogleAuthProvider,
+  signInWithPopup,
 } from "firebase/auth";
-
-import {getFirestore,doc,collection,updateDoc,getDocs} from "@firebase/firestore";
-import {db} from "./../../fireBase";
-import {createUserDocument} from "./../../fireBase";
-import {auth} from "./../../fireBase";
-import { getDatabase, ref, onValue} from "firebase/database";
-export const logIn=createAsyncThunk("auth/logIn", async  function (details){
-        const email=details.email , password=details.password;
-    try {      
-        const {user}=await signInWithEmailAndPassword(auth,email,password);
-         localStorage.setItem(
-      "user",
-      JSON.stringify(user ? user : null)
-    );
-
-
+import {
+  getFirestore,
+  doc,
+  collection,
+  updateDoc,
+  getDocs,
+} from "@firebase/firestore";
+import { db } from "./../../fireBase";
+import { createUserDocument } from "./../../fireBase";
+import { auth } from "./../../fireBase";
+// import { getDatabase, ref, onValue} from "firebase/database";
 
 export const logIn = createAsyncThunk("auth/logIn", async function (details) {
-    const email = details.email, password = details.password;
-    try {
-        const { user } = await signInWithEmailAndPassword(auth, email, password);
-        localStorage.setItem(
-            "user",
-            JSON.stringify(user ? user : null)
-        );
-        return user;
-    } catch (error) {
-        console.log(error.response);
-    }
+  const email = details.email,
+    password = details.password;
+  try {
+    const { user } = await signInWithEmailAndPassword(auth, email, password);
+    localStorage.setItem("user", JSON.stringify(user ? user : null));
+    return user;
+  } catch (error) {
+    console.log(error.response);
+  }
 });
 export const signUp = createAsyncThunk("auth/signUp", async function (details) {
-    const email = details.email, password = details.password;
-    try {
-        const { user } = await createUserWithEmailAndPassword(auth, email, password);
-        console.log(user);
-           localStorage.setItem(
-      "user",
-      JSON.stringify(user ? user : null)
+  const email = details.email,
+    password = details.password;
+  try {
+    const { user } = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
     );
-           createUserDocument(user,{address:"",email:user.email,latitude:"",longitude:"",} ); 
-return user;
-    } catch (error) {
-        console.log(error.response);
-    }
+    console.log(user);
+    localStorage.setItem("user", JSON.stringify(user ? user : null));
+    createUserDocument(user, {
+      address: "",
+      email: user.email,
+      latitude: "",
+      longitude: "",
     });
-  export const getPumpUserData=createAsyncThunk("auth/getPumpUserData",async(user)=>{
-      console.log("triggered");
-      const userRef=collection(db,"users");
-const data=await getDocs(userRef);
-const userDataObj=data.docs.map(user=>({...user.data(),id:user.id})).filter(item=>item.id===user.uid)[0];
-console.log(userDataObj);
-    return userDataObj ; 
-  });
-export const updateThePumpData=createAsyncThunk("auth/createThePumpData",async(details)=>{
-    const userDoc=doc(db,"users",details.uid)
-    // so details.data is basically a object needs to be updated with the help of this function  
-    const newFields=details.data
-    await updateDoc(userDoc,newFields);
+    return user;
+  } catch (error) {
+    console.log(error.response);
+  }
 });
+export const getPumpUserData = createAsyncThunk(
+  "auth/getPumpUserData",
+  async (user) => {
+    console.log("triggered");
+    const userRef = collection(db, "users");
+    const data = await getDocs(userRef);
+    const userDataObj = data.docs
+      .map((user) => ({ ...user.data(), id: user.id }))
+      .filter((item) => item.id === user.uid)[0];
+    console.log(userDataObj);
+    return userDataObj;
+  }
+);
+export const updateThePumpData = createAsyncThunk(
+  "auth/createThePumpData",
+  async (details) => {
+    const userDoc = doc(db, "users", details.uid);
+    // so details.data is basically a object needs to be updated with the help of this function
+    const newFields = details.data;
+    await updateDoc(userDoc, newFields);
+  }
+);
 
-const initialState={
-    currentUser:localStorage.getItem("user")
-      ? JSON.parse(localStorage.getItem("user"))
-      : null,
-    loading:false,
-    pumpUser:{}
-    }
+const initialState = {
+  currentUser: localStorage.getItem("user")
+    ? JSON.parse(localStorage.getItem("user"))
+    : null,
+  loading: false,
+  pumpUser: {},
+};
 
 const authSlice = createSlice({
-    name: "auth",
-    initialState,
-    reducers:{},
-    extraReducers(builder){
-        builder
-   .addCase(logIn.fulfilled,(state,action)=>{
-       state.currentUser=action.payload;
-   })
-   .addCase(signUp.fulfilled,(state,action)=>{
-       state.currentUser=action.payload;
-})
-.addCase(getPumpUserData.fulfilled,(state,action)=>{
-    state.pumpUser=action.payload
-})
-    },
-})
-export const { authStateChange } = authSlice.actions
+  name: "auth",
+  initialState,
+  reducers: {},
+  extraReducers(builder) {
+    builder
+      .addCase(logIn.fulfilled, (state, action) => {
+        state.currentUser = action.payload;
+      })
+      .addCase(signUp.fulfilled, (state, action) => {
+        state.currentUser = action.payload;
+      })
+      .addCase(getPumpUserData.fulfilled, (state, action) => {
+        state.pumpUser = action.payload;
+      });
+  },
+});
+export const { authStateChange } = authSlice.actions;
 export default authSlice.reducer;
