d37c1b6355763c14ad1a56a392d0af483f010d7e
arjun <arjun2000raj@gmail.com>
2022-05-09 10:51:53 +0530

    create : updatepump and update machine

--- src/Pages/UpdatePump/UpdatePump.jsx
+++ src/Pages/UpdatePump/UpdatePump.jsx
@@ -1,8 +1,46 @@
-import React from 'react'
-
+import GoogleMapReact from "google-map-react"
 const UpdatePump = () => {
+
   return (
-    <div>UpdatePump</div>
+    <section className="text-gray-600 body-font relative">
+      <div className="container px-5 py-24 mx-auto">
+        <div className="flex flex-col text-center w-full mb-12">
+          <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Update Pump</h1>
+          <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Update or add new pumps</p>
+        </div>
+        <div className="lg:w-1/2 md:w-2/3 mx-auto">
+          <div className="flex flex-wrap -m-2">
+            <div className="p-2 w-1/2">
+              <div className="relative">
+                <label for="name" className="leading-7 text-sm text-gray-600">Name</label>
+                <input type="text" id="name" name="name" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-green-500 focus:bg-white focus:ring-2 focus:ring-green-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+              </div>
+            </div>
+            <div className="p-2 w-1/2">
+              <div className="relative">
+                <label for="email" className="leading-7 text-sm text-gray-600">Address</label>
+                <input type="text" id="email" name="email" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-green-500 focus:bg-white focus:ring-2 focus:ring-green-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+              </div>
+            </div>
+            <div className="p-2 w-1/2">
+              <div className="relative">
+                <label for="name" className="leading-7 text-sm text-gray-600">Vacant</label>
+                <input type="number" id="name" name="name" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-green-500 focus:bg-white focus:ring-2 focus:ring-green-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+              </div>
+            </div>
+            <div className="p-2 w-1/2">
+              <div className="relative">
+                <label for="email" className="leading-7 text-sm text-gray-600">Occupied</label>
+                <input type="number" id="email" name="email" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-green-500 focus:bg-white focus:ring-2 focus:ring-green-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+              </div>
+            </div>
+            <div className="p-2 w-full">
+              <button className="flex mx-auto text-white bg-green-500 border-0 py-2 px-8 focus:outline-none hover:bg-green-600 rounded text-lg">Submit</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
